860c6047640a0eaff4332e51dfb3f8559b767483
Torgny Bjers <torgny.bjers@gmail.com>
2018-06-25 17:07:04 -0400

    switch to newer version of nvm

--- ellipsis.sh
+++ ellipsis.sh
@@ -9,7 +9,7 @@ pkg.install() {
       brew install watchman
       ;;
   esac
-  utils.run_installer 'https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh'
+  utils.run_installer 'https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh'
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   nvm install lts/argon
